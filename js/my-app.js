96fb6376bbbf814c4b09ad445252c1c8a82653ae
krishnatwc <33319559+krishnatwc@users.noreply.github.com>
2019-11-25 15:56:28 +0530

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -45,9 +45,9 @@ var mainView = TripcollectionApps.addView('.view-main', {
 });
 
 
-var RequestURL ='//www.adivaha.com/demo/MobAppRequest';
-var TPHotelUrl ='//hotel.tripcollection.com/hotels';
-var TPFlightUrl ='//flight.tripcollection.com/flights';
+var RequestURL ='https://www.adivaha.com/demo/MobAppRequest';
+var TPHotelUrl ='https://hotel.tripcollection.com/hotels';
+var TPFlightUrl ='https://flight.tripcollection.com/flights';
 var marker='250960';
 
 TripcollectionApps.onPageInit('index', function (page) {
